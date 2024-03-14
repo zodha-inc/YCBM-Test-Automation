0521604ea02f0e2a931b5dc41b8a6644791f14eb
asheetal-akadam <asheetal.akadam@gmail.com>
2024-03-14 14:50:58 -0400

    new test case added

--- src/main/java/pages/BookingPage.java
+++ src/main/java/pages/BookingPage.java
@@ -3,7 +3,6 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 import java.util.List;
 
 public class BookingPage extends WebPage {
@@ -15,38 +14,69 @@ public class BookingPage extends WebPage {
     protected List <WebElement> bookingActionIconButton;
 
     @FindBy(css = "div[class='menus_menuPane__28hfJ']>div:nth-child(4)")
-    protected WebElement bookingActionIconOptions;
+    protected WebElement bookingActionIconCancleOption;
 
     @FindBy(css = "div[style='margin-top: var(--sizes-x3); display: flex; justify-content: flex-end; gap: var(--sizes-x1); flex-wrap: wrap;']>button:nth-child(2)")
     protected WebElement bookingCancleButton;
 
     @FindBy(css = "tbody[class='Table_TableBody__1MlII']>tr >td:nth-child(7)")
-    protected List <WebElement> bookingTableStatusOption;
+    protected List <WebElement> bookingEventStatusOption;
+
+    @FindBy(css = "aside>div[class='BookingControls_controls__2As7S']>button[aria-label='Close modal']")
+    protected WebElement cancleBookingPopupCloseButton;
+
+    @FindBy(css = "div[class='clickout']>div>div>div:nth-child(2)>div[class='menus_menuIcon__XSyIj']")
+    protected WebElement bookingActionIconRebookOption;
+
+    @FindBy(css = "div[class='BookingUpdateDialog_profileCard__QpkzI']>div>div:nth-child(3)>button")
+    protected WebElement bookingRebookButton;
 
     public BookingPage(WebDriver driver) {
         super(driver);
     }
 
-    public void goToCancleBooking() {
+
+    public void cancleBooking() {
         bookingPageButton.click();
         sleepInSeconds(2);
 
-        for (int i = 0; i < bookingTableStatusOption.size(); i++) {
-
-            String status = bookingTableStatusOption.get(i).getText();
-
+        for (int i = 0; i < bookingEventStatusOption.size(); i++) {
+            String status = bookingEventStatusOption.get(i).getText();
             if (status.equals("Confirmed")) {
-                bookingActionIconButton.get(i).click();
+                WebElement actionIconButton = bookingActionIconButton.get(i);
+                clickElementWithJs(driver,actionIconButton);
                 sleepInSeconds(2);
-                bookingActionIconOptions.click();
+                bookingActionIconCancleOption.click();
                 sleepInSeconds(2);
                 bookingCancleButton.click();
                 sleepInSeconds(2);
+                cancleBookingPopupCloseButton.click();
+                sleepInSeconds(3);
             }
         }
     }
 
+    public void rebookCancelledBooking() {
+        bookingPageButton.click();
+        sleepInSeconds(2);
 
+
+        for (int i = 0; i < bookingEventStatusOption.size(); i++) {
+            String status = bookingEventStatusOption.get(i).getText();
+            if (status.equals("Cancelled")) {
+                WebElement actionIconButton = bookingActionIconButton.get(i);
+                clickElementWithJs(driver,actionIconButton);
+                sleepInSeconds(2);
+                bookingActionIconRebookOption.click();
+                sleepInSeconds(2);
+                bookingRebookButton.click();
+                sleepInSeconds(2);
+
+            }
+
+        }
+
+    }
 
 }
 
--- src/test/java/core/BookingPageTest.java
+++ src/test/java/core/BookingPageTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.BookingPage;
 
 import java.time.Duration;
@@ -16,15 +17,24 @@ public class BookingPageTest extends BaseTest {
     protected BookingPage bookingPage;
 
     @Test(priority = 0)
-    public void goToCancleBookingOption() {
+    public void cancleBookingTest() {
 
         bookingPage = PageFactory.initElements(driver,BookingPage.class);
-        bookingPage.goToCancleBooking();
+        bookingPage.cancleBooking();
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement toastMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.toaster div.styledWrapper.toast-content")));
         String actualToastText = toastMessage.getText();
         Assert.assertEquals(actualToastText ,"The booking has been cancelled.");
+    }
+
+    @Test(priority = 1)
+    public void rebookCancelledBookingTest() {
+
+        bookingPage = PageFactory.initElements(driver,BookingPage.class);
+        bookingPage.rebookCancelledBooking();
+
+
     }
 
 }
