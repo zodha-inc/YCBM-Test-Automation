26a23a245a40ccf88ba92a39bca0d710485a3b26
Alisha Chopra <alisha.s.chopra@gmail.com>
2024-03-15 16:07:28 -0400

    updated delete specific booking page function implementation

--- src/test/java/core/DashboardPageTest.java
+++ src/test/java/core/DashboardPageTest.java
@@ -1,19 +1,46 @@
 package core;
 
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import pages.DashboardPage;
 
 @Test(priority = 0)
 public class DashboardPageTest extends BaseTest {
 
+    protected DashboardPage dashboardPage;
     @BeforeClass
     public void classSetup() {
-
+        dashboardPage = PageFactory.initElements(driver, DashboardPage.class);
     }
 
     @Test(priority = 1)
     public void successfulLogin() {
 
+    }
+
+    @Test(priority = 2)
+    public void searchSpecificBookingPageTest() {
+        dashboardPage.searchParticularBookingPage();
+        if(dashboardPage.getSearchedBookingPageText().contains("no booking")) {
+            Assert.assertEquals(dashboardPage.getNoBookingPageText(), dashboardPage.getSearchedBookingPageText());
+        }else {
+            Assert.assertEquals(dashboardPage.getBookingPageToSearchValue(),
+                    dashboardPage.getSearchedBookingPageText());
+        }
+
+    }
+
+    @Test(priority = 3)
+    public void deleteSpecificBookingPageTest() {
+        dashboardPage.deleteSpecificBookingPage("test124");
+        int sizeBeforeDelete = dashboardPage.getBookingPagesSize();
+        int sizeAfterDelete = dashboardPage.getBookingPagesSizeAfterDelete();
+        if(sizeBeforeDelete == sizeAfterDelete) {
+            Assert.assertFalse(sizeBeforeDelete > sizeAfterDelete, "specific booking page to delete not found");
+        } else {
+            Assert.assertTrue(sizeBeforeDelete > sizeAfterDelete, "Specific booking page was successfully deleted.");
+        }
     }
 }
